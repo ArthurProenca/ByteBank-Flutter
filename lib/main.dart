589f59e17e710d6f8fde3433961db3eb6c53b562
Arthur Proença <m270800>
2022-02-16 22:56:35 -0300

    Criação de um formulário e refactor encapsulando os conteúdos, simplesmente BRUTAL!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,79 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTransferencia(),
-          appBar: AppBar(
-            title: const Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-              onPressed: () {}, child: const Icon(Icons.add)),
-        ),
+void main() => runApp(const ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
       ),
     );
+  }
+}
 
-/*Iremos criar um widget só nosso!
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(16.0), //Você consegue deixar a seu gosto.
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    labelText: 'Número da conta', hintText: '0000-0'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0), //Você consegue deixar a seu gosto.
+              child: TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: 'Valor',
+                    hintText: '0,00'),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            RaisedButton(
+              child: const Text('Confirmar'),
+              onPressed: () {},
+            )
+          ],
+        ));
+  }
+}
+
+/*  Iremos criar um widget só nosso!
   * StatefulWidget = dynamic
   * StatelessWidget = static
  */
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0, 1000)),
-        ItemTransferencia(Transferencia(200.0, 2000)),
-        ItemTransferencia(Transferencia(300.0, 3000)),
-      ],
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
     );
   }
 }
